74f1ce71d7f12b6e54e8a67aba75a3ba186419c6
Taij-the-neko <taijm69@gmail.com>
2024-08-04 09:51:00 +0400

    Add files via upload

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,40 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {
+      renderer = {
+        icons = {
+          glyphs = {
+            default = "",
+            symlink = "",
+            git = {
+              unstaged = "✗",
+              staged = "✓",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "★",
+              deleted = "",
+              ignored = "◌",
+            },
+            folder = {
+              arrow_open = "",
+              arrow_closed = "",
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+              symlink_open = "",
+            },
+          },
+        },
+      },
+    }
+    vim.keymap.set('n', '<Space>e', ':NvimTreeToggle<CR>')
+  end,
+}
